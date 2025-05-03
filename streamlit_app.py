6247221c0ae081e3e4eaaebe6674d5cc29dbb3c4
givemeurdad <btbhavsar28@gmail.com>
2025-05-03 16:58:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,9 @@ target_mapper = {'Adelie': 0,
 def target_encode(val):
   return target_mapper[val]
 
+y = y_raw.apply(target_encode)
+y
+
 with st.expander('Input Features'):
   st.write('**Input Penguin**')
   input_df
