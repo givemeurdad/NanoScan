b51a4431eb062efe2ec6759fd8bd2d2058569b29
givemeurdad <btbhavsar28@gmail.com>
2025-05-03 16:55:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X = df.drop('species', axis=1)
-  X
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
   st.write('**y**')
-  y = df.species
-  y
+  y_raw = df.species
+  y_raw
 
 
 with st.expander('Data visualization'):
@@ -41,19 +41,25 @@ with st.sidebar:
           'body_mass_g': body_mass_g,
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 
-  #encode X
-  encode = ['island', 'sex']
-  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-  input_row = df_penguins[:1]
+#encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
 
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode[val]:
+  return target_mapper[val]
 
 with st.expander('Input Features'):
   st.write('**Input Penguin**')
   input_df
   st.write('**Combined penguin data**')
   input_penguins
-  st.write('Encoded input penguin')
+  st.write('**Encoded input penguin**')
   input_row
