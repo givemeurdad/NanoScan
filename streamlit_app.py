f4f2ee815d4792d13334b6bbb9056210d8c4f13e
givemeurdad <btbhavsar28@gmail.com>
2025-05-03 20:38:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
 st.title('NanoScan')
-image = Image.open('img/streamlit-mark-light.png')
+image = Image.open('NanoScan.jpg')
 st.image(image, caption = NanoScan, width = 400, channels = "RGB")
 
 st.info('This software allows you to get a basic screening for any disorders')
